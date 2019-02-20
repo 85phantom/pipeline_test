ddb8957a6fc6cc7ced849bb9b123078e2f509a60
柯博文 <oxox850502@gmail.com>
2019-02-20 12:03:00 +0800

    test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ describe('sum', () => {
   // 測試算出來的平均是不是 2.5
   it('should return the sum of two numbers', done => {
     const n1 = 10, n2 = 5;
-    const expected = 15;
+    const expected = 12;
     
     const result = sum(n1, n2);
     assert.equal(result, expected);
